6751d8083d9d4730b17574df003adaeb562ca540
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-07-04 19:25:52 +1000

    prevent default on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ let submitButton = document.querySelector("#submit-btn");
 let playerName = document.querySelector("#initials");
 let playerHighScore = document.querySelector("#score");
 
-function saveHighScores() {
+function saveHighScores(event) {
+  event.preventDefault();
   let highScoreSave = {
     playerNameSave: playerName.value,
     playerHighScoreSave: playerHighScore.value,
